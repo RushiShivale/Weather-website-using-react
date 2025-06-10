cfee12ff7190b567bb843f40c5e554c1d6b467e1
RushiShivale <rushikeshshivale38@gmail.com>
2025-06-10 23:59:32 +0530

    analylitics upload

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import WeatherApp from './WeatherApp'
 import './App.css'
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <div className="App">
       <WeatherApp />
+
+      <Analytics />
     </div>
   )
 }
